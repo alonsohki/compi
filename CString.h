ae57a8cd3183a9d6aa6b507a83e4497a73ac1105
Alberto Alonso <rydencillo@gmail.com>
2010-11-19 17:48:27 +0000

    Añadida la clase CString.
    git-svn-id: svn://aeon.xenmax.net/ryden/compi/2010_2011@44 1d2d9ec6-5410-49c2-88f3-5269144744df

---
+++ CString.h
@@ -0,0 +1,81 @@
+/* 
+ * File:   CString.h
+ * Author: Alberto Alonso
+ *
+ */
+
+#ifndef CSTRING_H
+#define	CSTRING_H
+
+#include <cstdarg>
+#include <string>
+#include "util.h"
+
+class CString
+{
+private:
+    std::string     m_str;
+public:
+    CString () {};
+    CString ( const char* str ) : m_str ( str ) {}
+    CString ( const std::string& str ) : m_str ( str ) {}
+    CString ( const CString& Right ) : m_str ( Right.m_str ) {}
+
+    CString& operator= ( const CString& Right )
+    {
+        m_str = Right.m_str;
+        return *this;
+    }
+    CString& operator= ( const char* str )
+    {
+        m_str.assign ( str );
+        return *this;
+    }
+    CString& operator= ( const std::string& str )
+    {
+        m_str = str;
+        return *this;
+    }
+
+    const char* operator* () const
+    {
+        return m_str.c_str ();
+    }
+    
+    operator const char*() const
+    {
+        return m_str.c_str ();
+    }
+
+    CString operator|| ( const CString& Right ) const
+    {
+        CString ret ( *this );
+        ret.m_str.append ( Right.m_str );
+        return ret;
+    }
+
+    CString operator|| ( const char* Right ) const
+    {
+        CString ret ( *this );
+        ret.m_str.append ( Right );
+        return ret;
+    }
+};
+
+class Format : public CString
+{
+public:
+    explicit Format ( const char* format, ... )
+    {
+        va_list vl;
+        char temp [ 512 ];
+        va_start ( vl, format );
+        vsnprintf ( temp, NUMELEMS(temp), format, vl );
+        va_end ( vl );
+
+        *static_cast<CString *> ( this ) = temp;
+    }
+};
+
+#endif	/* CSTRING_H */
+
